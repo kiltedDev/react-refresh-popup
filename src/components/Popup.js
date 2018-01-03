35e5a00707eab416d9c0f205edbeeb8ea78e1c27
Thomas Wilson <twilson.spqr@gmail.com>
2018-01-02 23:48:25 -0500

    unjanked css

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -11,15 +11,14 @@ const Popup = props => {
   }
 
   return(
-    <div>
-      <div className="boxed">
-        <i className = "fa fa-times" areia-hidden="true" onClick={handleExit}/>
+    <div id="element_to_pop_up">
+      <div id="boxed">
+        <i className = "fa fa-times" onClick={handleExit}/>
       </div>
-      <div id="element_to_pop_up">
+
       <h3>You won tickets to see {props.artist}!!!</h3>
-      </div>
-        <p>Please enter your email so we can send you the tickets</p>
-        <form>
+      <p>Please enter your email so we can send you the tickets</p>
+        <form onSubmit={submit}>
           <label htmlFor="email">Name:</label>
           <input id="field" type="text" name="email" placeholder="Your email here:"/>
           <input id="submission" type="submit" value="CLAIM YOUR PRIZE HERE"/>
